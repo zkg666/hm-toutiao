1fa38d9af5d9c3ffa17c7a28cd2517189e787fb4
zkg666 <90459567@qq.com>
2019-10-25 15:15:11 +0800

    初始化基本完成

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入element-ui第三方的包子
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 使用饿了么
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
